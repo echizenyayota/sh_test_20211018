4c237a2b839c899dadb2d9bb6a539f76732430ee
echizenyayota <virtualmisso30@gmail.com>
2021-10-20 16:06:29 +0900

    Insert Console.log (make sure a price)_2

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ const Index = () => {
         }
       }).then((data) => {
         // console.log(data.variants.edges[0].node.price);
-        // console.log(product.variants.edges[0].node.price);
+        // console.log(products.variants.edges[0].node.price);
         console.log(products);
         console.log(data);
         console.log('Update Product', count, data);
